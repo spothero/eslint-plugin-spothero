682a1a3f4e3a2cd5dd8a080d498f510971909575
boiarqin <boiarqin@gmail.com>
2018-12-03 15:08:03 -0600

    style: Update documentation and fix code inconsistencies

--- docs/rules/ternary-parentheses.md
+++ docs/rules/ternary-parentheses.md
@@ -1,1 +1,41 @@
-# Require Ternary Expressions to have condition enclosed by parentheses (ternary-parentheses)
+# Require ternary expressions to have condition enclosed by parentheses (ternary-parentheses)
+
+This addresses a stylistic issue. Surrounding the condition in parentheses promotes faster scanning of code and identification of the condition within a ternary expression.
+
+```js
+/* eslint-env es6 */
+
+const qux = (foo) ? bar : baz;
+```
+
+## Rule Details
+
+This rule enforces parenthese around the condition in a ternary expression.
+
+Examples of **incorrect** code for this rule:
+
+```js
+/*eslint ternary-parentheses: "error"*/
+/* eslint-env es6 */
+
+const qux = foo ? bar : baz;
+
+const qux = foo
+    ? bar
+    : baz;
+```
+
+Examples of **correct** code for this rule:
+
+```js
+/*eslint ternary-parentheses: "error"*/
+/* eslint-env es6 */
+
+const qux = (foo) ? bar : baz;
+
+const qux = (foo && fred) ? bar : baz;
+
+const qux = (foo)
+    ? bar
+    : baz;
+```
--- tests/lib/rules/ternary-parentheses.js
+++ tests/lib/rules/ternary-parentheses.js
@@ -1,41 +1,44 @@
 /**
- * @fileoverview Tests for complexity rule.
- * @author Boiar Qin
+ * @fileoverview Tests for ternary-parentheses rule.
  */
 
-"use strict";
+'use strict';
 
 //------------------------------------------------------------------------------
 // Requirements
 //------------------------------------------------------------------------------
 
-const rule = require("../../../lib/rules/ternary-parentheses"),
-    RuleTester = require("eslint/lib/testers/rule-tester");
+const rule = require('../../../lib/rules/ternary-parentheses');
+const RuleTester = require('eslint/lib/testers/rule-tester');
 
 //------------------------------------------------------------------------------
 // Helpers
 //------------------------------------------------------------------------------
 
-const ruleTester = new RuleTester();
+const ruleTester = new RuleTester({
+    parserOptions: {
+        ecmaVersion: 9
+    }
+});
 
-ruleTester.run("ternary-parentheses", rule, {
+ruleTester.run('ternary-parentheses', rule, {
     valid: [
-        "var o = (a) ? b : c;",
-        "var o = (a)\n ? b\n : c;",
-        "var o = (a && z) ? b : c;",
-        "var o = (a && z)\n ? b\n : c;",
-        "var o = (a\n && z)\n ? b\n : c;",
-        "var o = (a && y || z) ? b : c;",
-        "var o = (a && (y || z))\n ? b\n : c;"
+        'const o = (a) ? b : c;',
+        'const o = (a)\n ? b\n : c;',
+        'const o = (a && z) ? b : c;',
+        'const o = (a && z)\n ? b\n : c;',
+        'const o = (a\n && z)\n ? b\n : c;',
+        'const o = (a && y || z) ? b : c;',
+        'const o = (a && (y || z))\n ? b\n : c;'
     ],
     invalid: [
         {
-            code: "var o = a ? b : c;",
-            errors: [{ message: "Expected parentheses around condition of ternary expression." }]
+            code: 'const o = a ? b : c;',
+            errors: [{message: 'Expected parentheses around condition of ternary expression.'}]
         },
         {
-            code: "var o = a\n ? b\n : c;",
-            errors: [{ message: "Expected parentheses around condition of ternary expression." }]
+            code: 'const o = a\n ? b\n : c;',
+            errors: [{message: 'Expected parentheses around condition of ternary expression.'}]
         }
     ]
 });
